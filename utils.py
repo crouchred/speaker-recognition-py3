7812a6ae04afeaa7bf8ff2f6d6df1ab4b65d46b5
chenxuyuan <crouchred@gmail.com>
2018-09-12 14:22:41 +0800

    Convert to mono if stereo

--- utils.py
+++ utils.py
@@ -2,5 +2,7 @@ from scipy.io import wavfile
 
 def read_wav(fname):
     fs, signal = wavfile.read(fname)
-    assert len(signal.shape) == 1, "Only Support Mono Wav File!"
+    if len(signal.shape) != 1:
+        print("convert stereo to mono")
+        signal = signal[:,0]
     return fs, signal
